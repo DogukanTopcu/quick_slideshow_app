d28bbcd19e62e03c5141e2cf22c4471b37b7ced3
Doğukan Topçu <dogukantopcu35@gmail.com>
2024-04-14 18:20:39 +0300

    editor page editing area and image, music, text area layout implemented

--- lib/screen/editor.dart
+++ lib/screen/editor.dart
@@ -19,6 +19,8 @@ class _EditorState extends State<Editor> {
 
   int selectedImageIndex = -1;
 
+  bool isPlaying = false;
+
   void _selectImages() async {
     final List<XFile> selectedImages = await ip.pickMultiImage();
 
@@ -73,56 +75,151 @@ class _EditorState extends State<Editor> {
               flex: 6,
               child: Container(
                 color: Colors.red,
-                child: const Center(
-                  child: Text("data"),
+                child: Container(
+                  color: Colors.black,
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.width,
+                  child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Bum"),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            // Slideshow play buttons
+            Expanded(
+              flex: 1,
+              child: Container(
+                  color: Colors.white,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.arrow_left,
+                            color: Colors.black,
+                          )),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.keyboard_double_arrow_left,
+                                color: Colors.black,
+                              )),
+                          IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                !isPlaying ? Icons.play_arrow : Icons.pause,
+                                color: Colors.black,
+                              )),
+                          IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.keyboard_double_arrow_right,
+                                color: Colors.black,
+                              )),
+                        ],
+                      ),
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.arrow_right,
+                            color: Colors.black,
+                          )),
+                    ],
+                  )),
+            ),
+            // Editor: Image, music and text area
+            Expanded(
+              flex: 4,
+              child: Container(
+                alignment: Alignment.centerLeft,
+                child: ListView(
+                  scrollDirection: Axis.vertical,
+                  children: [
+                    Container(
+                        color: Colors.black,
+                        height: 144,
+                        child: imageFileList.isEmpty
+                            ? const Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(Icons.image),
+                                  Text("Add Photo"),
+                                ],
+                              )
+                            : ListView.builder(
+                                scrollDirection: Axis.horizontal,
+                                itemCount: imageFileList.length,
+                                itemBuilder: (BuildContext context, int index) {
+                                  if (index == imageFileList.length - 1) {
+                                    return Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        Container(
+                                          width: 98,
+                                          height: 98,
+                                          margin: const EdgeInsets.all(8),
+                                          child: Image.file(
+                                            File(imageFileList[index].path),
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                      ],
+                                    );
+                                  }
+                                  return Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        Container(
+                                          width: 98,
+                                          height: 98,
+                                          margin: const EdgeInsets.all(8),
+                                          child: Image.file(
+                                            File(imageFileList[index].path),
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                        const Icon(
+                                            Icons.keyboard_double_arrow_right),
+                                      ]);
+                                },
+                              )),
+                    Container(
+                      color: Colors.black54,
+                      height: 98,
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.music_note),
+                          Text("Add Music"),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      color: Colors.black,
+                      height: 98,
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.text_fields),
+                          Text("Add Text"),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
             Expanded(
               flex: 1,
-              child: Container(
-                color: Colors.green,
-                child: const Center(
-                  child: Text("data"),
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 3,
-              child: Container(
-                color: Colors.blue,
-                child: Center(
-                    child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: imageFileList.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    if (index == imageFileList.length - 1) {
-                      return Container(
-                        margin: const EdgeInsets.all(8),
-                        child: Image.file(
-                          File(imageFileList[index].path),
-                          fit: BoxFit.cover,
-                        ),
-                      );
-                    }
-                    return Row(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Container(
-                            margin: const EdgeInsets.all(8),
-                            child: Image.file(
-                              File(imageFileList[index].path),
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          const Icon(Icons.arrow_right_alt_outlined),
-                        ]);
-                  },
-                )),
-              ),
-            ),
-            Expanded(
-              flex: 2,
               child: Container(
                 color: Colors.orange,
                 child: Center(
