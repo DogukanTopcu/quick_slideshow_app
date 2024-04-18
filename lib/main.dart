888121099936496a8c3c7b999c2eb6c2ad837acc
Doğukan Topçu <dogukantopcu35@gmail.com>
2024-04-18 21:02:34 +0300

    add todos

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:quick_slideshow/home.dart';
 
+// TODOS:
+/*
+ 1. Ekran seçeneklerinin oluşturulması
+ 2. Fotoğrafların ekrana oturtulması
+ 3. Fotoğrafın ekrandaki konumunun el ile ayarlanabilmesi
+ 4. Fotoğraf Editleme Ekranı
+ 5. Ses parçası yükleme butonu
+ 6. Slide Geçişleri
+ 7. Editor'e zaman çizelgesinin eklenmesi
+ 8. Play butonuna basıldığında video'nun akması
+ 9. Text Alanının Eklenmesi
+10. Çizim Alanının Eklenmesi
+11. Kayıt sistemi
+12. Ana Sayfanın düzenlenmesi
+13. Export işlemleri
+*/
+
 void main() {
   runApp(const MyApp());
 }
